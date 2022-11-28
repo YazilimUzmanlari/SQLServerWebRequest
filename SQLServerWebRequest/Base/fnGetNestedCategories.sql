0d0b4d20eab659bad2b3f55e1c155603bf2032a3
Hamit Serbest <hamit.serbest@bulutuygulamalari.com>
2022-11-28 22:09:18 +0300

    Base Stored Procedures and  Functions

---
+++ SQLServerWebRequest/Base/fnGetNestedCategories.sql
@@ -0,0 +1,31 @@
+﻿CREATE FUNCTION [dbo].[fnGetNestedCategories]
+(
+	@jsonData nvarchar(max)
+)
+Returns @Returned Table(Id int, ParentId int, Name nvarchar(max))
+AS
+BEGIN
+	WITH NestedCategories(Id, ParentId, Name, SubCategories) as
+	(
+		Select	Cast(json_value(Category.[value], '$.id') as int) Id, 
+				Cast(json_value(Category.[value], '$.parentId') as int) ParentId, 
+				Cast(json_value(Category.[value], '$.name') as nvarchar(max)) Name, 
+				Cast(json_query(Category.[value], '$.subCategories') as nvarchar(max)) SubCategories
+		From	OPENJSON(@jsonData, '$.categories') Category
+		union all
+		Select	Cast(json_value(SubCategories.[value], '$.id') as int) Id, 
+				Cast(json_value(SubCategories.[value], '$.parentId') as int) ParentId, 
+				Cast(json_value(SubCategories.[value], '$.name') as nvarchar(max)) Name, 
+				Cast(json_query(SubCategories.[value], '$.subCategories') as nvarchar(max)) SubCategories
+		From	NestedCategories
+			outer apply openjson(NestedCategories.SubCategories) SubCategories
+		Where	NestedCategories.SubCategories is not null or NestedCategories.SubCategories <> ''
+	)
+	Insert Into @Returned(Id, ParentId, Name)
+	Select	Id, 
+			ParentId, 
+			Name
+	From	NestedCategories
+	Where	Id is not null
+	return
+END
---
+++ SQLServerWebRequest/Base/sp_GetTCMBExchangeRates.sql
@@ -0,0 +1,36 @@
+﻿CREATE PROCEDURE [dbo].[sp_GetTCMBExchangeRates]
+	@date		datetime = null
+AS
+	declare @urlAddress		nvarchar(max)
+	declare @responseText	nvarchar(max)
+
+	if (@date is null)
+	begin
+		set @urlAddress = 'https://www.tcmb.gov.tr/kurlar/today.xml'
+	end
+	else
+	begin
+		set @urlAddress = 'https://www.tcmb.gov.tr/kurlar/' + format(@date, 'yyyyMM') + '/' + Format(@date, 'ddMMyyyy') + '.xml'
+	end
+
+	exec sp_GetHttpRequest @urlAddress, 'GET', 'text/xml', null, null, null, null, @responseText out
+
+	Create table #ResponseTable(ResponseXML xml)
+	
+	Insert Into #ResponseTable(ResponseXML)
+	Select Replace(Replace(@responseText,'<?xml version="1.0" encoding="UTF-8"?>',''),'<?xml-stylesheet type="text/xsl" href="isokur.xsl"?>','') 
+	
+	select	ResponseXML
+	From	#ResponseTable
+	
+	select	b.Data.value('@Kod', 'nvarchar(5)') CurrencyCode, 
+			b.Data.value('(CurrencyName/text())[1]', 'nvarchar(50)') CurrencyName, 
+			Convert(datetime, b.Data.value('../@Tarih', 'nvarchar(20)'),103) [Date], 
+			b.Data.value('(ForexBuying/text())[1]', 'float') ForexBuying,
+			b.Data.value('(ForexSelling/text())[1]', 'float') ForexSelling,
+			b.Data.value('(BanknoteBuying/text())[1]', 'float') BanknoteBuying,
+			b.Data.value('(BanknoteSelling/text())[1]', 'float') BanknoteSelling
+	From	#ResponseTable
+		Cross apply #ResponseTable.[ResponseXML].nodes('/Tarih_Date//Currency') b(Data)
+	drop table #ResponseTable
+RETURN 0
